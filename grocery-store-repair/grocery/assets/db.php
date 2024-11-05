392c39d4d174bf615ea825377e47e11c5e7d3311
xzhan117 <xzhan117@nyit.edu>
2024-11-05 09:00:56 +0800

    Add files via upload

---
+++ grocery-store-repair/grocery/assets/db.php
@@ -0,0 +1,9 @@
+<?php 
+    $con = new mysqli('localhost','grocery','qwer123','grocery');
+//for user informationa making available for all pages
+
+    $array = $con->query("select * from users where id ='$_SESSION[userId]'");
+    $user = $array->fetch_assoc();
+
+	$base_path = "http://localhost/dev.test/grocery";
+?>
